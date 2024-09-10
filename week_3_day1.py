c4a95ffaf2097aaa5e98b8766d70d96dfb35d313
Saucedo <ASaucedo44@cps.k12.il.us>
2024-09-10 15:03:16 -0500

    first commit

--- week_3_day1.py
+++ week_3_day1.py
@@ -50,29 +50,43 @@
 # # Review
 # create variables for the following :
 # 1. age
+age = 16 #integer variable
 # 2. name
+name = "Abby" #string variable
 # 3. song
+song = "Party in the USA" #string variable
 # 4. food
+food = "cookies" #string variable
 # 5. number
+number = 1000 #integer variable
 
 
 # #now include the variables you just made print in the following...
 
 
 # Once upon a time, there was a [age] old coder named [name].
-
+print("Once upon a time, there was a " + str(age) + " old coder named " + name + ".")
 
 # [name] liked to hum the song [song] while coding. It was so annoying that their teammates would throw [food] until [name] would stop singing.
-
+print(name + " liked to hum the song " + song + " while coding. It was so annoying that their classmates would throw " + food + " until " + name + " would stop singing.")
 
 # Still, [name] was the best coder on the team and could write [number] lines of code every day. Maybe [song] was [name]’s secret power?
 ##########################################################################################
 
+print("Still, " + name + " was the best coder on the team and could write " + str(number) + " lines of code everyday. Maybe " + song + " was " + name + "'s secret power? ")
 
 
 
+number1 = 100
+number2 = 200
+number3 = 300
+number4 = 400
+number5 = 500
 
+# print(str(number1) + ", " + str(number2) + ", " + str(number3) + ", " + str(number4) + ", " + str(number5) + "." )
 
+# f strings
+print(f"{number1},{number2},{number3},{number4},{number5}")
 
 
 
